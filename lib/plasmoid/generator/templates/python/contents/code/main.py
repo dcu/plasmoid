69be85e4a9813b84a2672800b858378bf0eb43aa
David A. Cuadrado <krawek@gmail.com>
2011-06-04 12:07:26 -0500

    improve python support
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- lib/plasmoid/generator/templates/python/contents/code/main.py
+++ lib/plasmoid/generator/templates/python/contents/code/main.py
@@ -1,22 +1,26 @@
-from PyQt4.QtCore import Qt
+# -*- coding: utf-8 -*-
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 from PyKDE4.plasma import Plasma
 from PyKDE4 import plasmascript
- 
+from PyKDE4.kdecore import *
+from PyKDE4.kdeui import *
+
 class <%= options[:project_name].capitalize %>(plasmascript.Applet):
-    def __init__(self,parent,args=None):
-        plasmascript.Applet.__init__(self,parent)
- 
-    def init(self):
-        self.setHasConfigurationInterface(False)
-        self.resize(125, 125)
-        self.setAspectRatioMode(Plasma.Square)
- 
-    def paintInterface(self, painter, option, rect):
-        painter.save()
-        painter.setPen(Qt.white)
-        painter.drawText(rect, Qt.AlignVCenter | Qt.AlignHCenter, "Hello Python!")
-        painter.restore()
- 
+	def __init__(self,parent,args=None):
+		plasmascript.Applet.__init__(self,parent)
+
+	def init(self):
+		self.setHasConfigurationInterface(False)
+		self.resize(375, 525)
+		self.setAspectRatioMode(Plasma.IgnoreAspectRatio)
+
+		self.layout = QGraphicsLinearLayout(Qt.Horizontal, self.applet)
+		self.webView = Plasma.WebView(self.applet)
+		self.webView.setUrl(KUrl("http://www.kde.org"))
+
+		self.layout.addItem(self.webView)
+		self.setLayout(self.layout)
+
 def CreateApplet(parent):
 	return <%= options[:project_name].capitalize %>(parent)
-
